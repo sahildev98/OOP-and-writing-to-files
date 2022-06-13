41fac30175137030813aae799636a9fde492b7fa
Sahil Ahmed <64647573+sahildev98@users.noreply.github.com>
2022-06-13 13:45:07 +0000

    this keyword used to reference a object of a class.

--- Animal.php
+++ Animal.php
@@ -4,9 +4,11 @@ class Animal{
   private $name;
   private $age;
   private $colour;
-
+  // Animal class constructor 
   public function __construct($name, $age, $colour){
-    
+    $this->name = $name;
+    $this->age = $age;
+    $this->colour = $colour; 
   }
 }
 
