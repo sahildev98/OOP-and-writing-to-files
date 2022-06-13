f170653f443d51a65c005e7ff56fa6ce3d4b44fd
Sahil Ahmed <64647573+sahildev98@users.noreply.github.com>
2022-06-13 14:18:58 +0000

    getters and setters created to set and get properties of class Animal.

--- Animal.php
+++ Animal.php
@@ -10,13 +10,31 @@ class Animal{
     $this->age = $age;
     $this->colour = $colour; 
   }
-
+  
+  /* getters and setters created. Setters functions created to create 
+  set attributes. Getter functions created to get the attributes.*/ 
   public function set_name($name){
     $this->name = $name;
   }
   
   public function get_name(){
     return $this->name;
+  }
+
+  public function set_age($age){
+    $this->age = $age;
+  }
+
+  public function get_age(){
+    return $this->$age;
+  }
+
+  public function set_colour($colour){
+    $this->colour = $colour;
+  }
+
+  public function get_colour{
+    return $this->$colour;
   }
 }
 
